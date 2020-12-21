63e56c9b29a9ba65375916042c17ea99763d6c1c
alison <barretalison@gmail.com>
2020-12-20 19:47:02 -0800

    Added api routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,22 @@ var router = express.Router();
 
 const users = require("../controllers/Admin/userController.js");
 const products = require("../controllers/Producteurs/productController.js");
+const cart = require("../controllers/Clients/cartController.js");
+const adresse = require("../controllers/Clients/adresseController.js");
+const order = require("../controllers/Clients/orderController.js")
 
 
 router.get('/users', users.findAll)
 
+router.post('/livraison', adresse.create)
+
+router.get('/:id/livraison', adresse.findByUser)
+
+
 router.get('/products', products.findAll)
 
+router.get('/:id/cart', cart.cart);
+
+router.post('/order', order.create);
 
 module.exports = router;
