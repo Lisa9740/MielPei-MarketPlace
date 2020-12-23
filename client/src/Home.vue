8da2cabf72285de73b2494f620d07fc89b9f9b12
alison <barretalison@gmail.com>
2020-12-23 03:08:20 -0800

    Added BestProducts components in home view (client side)

--- client/src/Home.vue
+++ client/src/Home.vue
@@ -1,7 +1,7 @@
 <template>
-
   <v-container>
-    <h1 class="mb-5 mt-5">Bienvenue à MielPéi! {{user}}</h1>
+    <h1 class="mb-5 mt-5">Bienvenue à MielPéi! <span v-if="user">{{user.firstName}} {{user.lastName}}</span></h1>
+
     <v-row>
       <v-col cols="6" class="pa-6">
         <MapExploitation/>
@@ -18,50 +18,12 @@
           </v-card-text>
         </v-card>
       </v-col>
-
     </v-row>
-
 
     <v-container>
       <h2 class="mt-8"> Meilleurs produits encore en stock</h2>
       <v-divider class="yellow-divider"></v-divider>
-      <v-row>
-        <v-col cols="12" sm="6" md="4" v-for="product in products[0]" :key="product.id">
-          <v-card>
-            <v-img
-                height="140"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img>
-
-              <v-card-title>{{ product.name }}</v-card-title>
-              <v-card-text>
-                <strong>{{ product.price }}  </strong> € •
-                <v-row
-                    align="center"
-                    class="mx-0"
-                >
-                  <v-rating
-                      :value="4.5"
-                      color="amber"
-                      dense
-                      half-increments
-                      readonly
-                      size="14"
-                  ></v-rating>
-
-                  <div class="grey--text ml-4">
-                    4.5 (413)
-                  </div>
-                </v-row>
-
-              </v-card-text>
-            <v-card-actions>
-              <CartButton :cartIcon="true" @click.native="addProductToCart(product)"/>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-
-      </v-row>
+      <BestProducts/>
     </v-container>
   </v-container>
 </template>
@@ -76,43 +38,17 @@
 </style>
 
 <script>
-import CartButton from "./components/CartButton";
-import {mapActions} from "vuex";
+import BestProducts from "@/components/BestProducts";
 import MapExploitation from "./components/MapExploitation";
 import userConfig from "@/utils/userConfig";
 
-
 export default {
   data: () => ({
-    user: userConfig.getUser()
+    user: JSON.parse(userConfig.getUser())
   }),
   components: {
-    CartButton,
-    MapExploitation
-  },
-  computed: {
-    products() {
-      return this.$store.state.products
-    },
-
-
-  },
-
-  methods: {
-    ...mapActions([
-      'addProductToCart',
-      'currentProduct',
-    ]),
-
-    addCurrentProduct(product) {
-      this.currentProduct(product);
-    },
-
-  },
-  mounted() {
-    this.$store.dispatch("getProducts");
-
-
+    MapExploitation,
+    BestProducts
   },
 }
 </script>
